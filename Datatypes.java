36388dc0eef8624d4148044cb37d6a168237da70
Abishek N <151867893+Abisheknesa@users.noreply.github.com>
2024-05-23 20:28:55 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,29 @@
+package newas;
+
+//Datatypes java
+
+public class Begin {
+
+	public static void main(String[] args) {
+		int a = 15;
+		char name='k';
+		String names="ABISHEK"; //string is a class in java
+		short age= 21;
+		float height =6_7;
+		double percenta=9_77;
+		if (age ==21)
+		{
+			System.out.println("true");
+			System.out.println(names+" age is "+age);
+
+		}
+		else
+		{
+			System.out.println("false");
+		}
+		
+		
+
+	}
+
+}
